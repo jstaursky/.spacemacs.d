acee062d2328a8727814b9a99318b441d39dffa1
jstaursky <jstaursky@gmail.com>
2019-03-21 08:10:46 -0400

    removed irony-eldoc, :( does not work with iedit.

--- layers/conf-c-c++/packages.el
+++ layers/conf-c-c++/packages.el
@@ -4,32 +4,21 @@
 
 (defconst conf-c-c++-packages
   '(
-    (irony
-     :location (recipe :fetcher github :repo "Sarcasm/irony-mode"))
-
-    (company-irony :toggle (configuration-layer/package-usedp 'company)
-     :location (recipe :fetcher github
-                       :repo "Sarcasm/company-irony"))
-
-    (company-irony-c-headers :toggle
-                             (configuration-layer/package-usedp 'company)
-     :location (recipe :fetcher github
-                       :repo "hotpxl/company-irony-c-headers"))
-
-    (flycheck-irony
-     :location (recipe :fetcher github
-                       :repo "Sarcasm/flycheck-irony"))
-
-    (clang-format
-     :location (recipe :fetcher github
-                       :repo "emacsorphanage/clang-format"))
+    company
+    irony
+    (company-irony           :requires company irony)
+    (company-irony-c-headers :requires company irony)
+    (flycheck-irony          :requires irony)
     )
   ) ;; conf-c-c++-packages
 
 ;; init functions
 ;;
-(defun conf-c-c++/init-clang-format ()
-  (use-package clang-format))
+
+(defun conf-c-c++/post-init-company ()
+  (spacemacs|add-company-backends
+    :backends (company-irony company-irony-c-headers) :modes c-mode-common)
+  )
 
 (defun conf-c-c++/init-irony ()
   (use-package irony
@@ -37,32 +26,25 @@
             (c-mode    . irony-mode)
             (objc-mode . irony-mode))
     :config (irony-cdb-autosetup-compile-options)
+    (spacemacs/set-leader-keys-for-major-mode
+      'c-mode "t" 'irony-get-type)
+    (spacemacs/set-leader-keys-for-major-mode
+      'c-mode ";" 'c-toggle-comment-style)
     ))
 
 (defun conf-c-c++/init-company-irony ()
   (use-package company-irony :after irony
-    :init    (add-to-list 'company-backends
-                          'company-irony)
-    :hook    irony-mode-hook
-    :config  (add-hook 'irony-mode-hook
-                       'company-irony-setup-begin-commands)
+    :config (company-irony-setup-begin-commands)
     ))
 
 (defun conf-c-c++/init-company-irony-c-headers ()
   (use-package company-irony-c-headers :after (irony company-irony)
-    :init (add-to-list 'company-backends
-                       'company-irony-c-headers)
-    :hook irony-mode-hook
     ))
 
 (defun conf-c-c++/init-flycheck-irony ()
-  (use-package flycheck-irony :after irony))
+    (use-package flycheck-irony :after irony
+      :init   (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)
+      :config (setq flycheck-clang-language-standard "c++11")
+      ))
 
-;; Post-init functions
-;;
-(defun conf-c-c++/post-init-flycheck-irony ()
-  (use-package flycheck-irony :after irony
-    :init   (add-hook  'flycheck-mode-hook
-                      #'flycheck-irony-setup)
-    :config (setq flycheck-clang-language-standard "c++11")
-    ))
+
